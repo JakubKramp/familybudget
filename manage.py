35b62016014ae6b4be9bf0ba3ffb065d9dd5dbf6
Jakub <jakramp96@gmail.com>
2024-07-17 01:56:45 +0200

    Set up project structure

--- manage.py
+++ manage.py
@@ -4,19 +4,35 @@ import os
 import sys
 
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "familybudget.settings")
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
-
+#!/usr/bin/env python
+# ruff: noqa
+import os
+import sys
+from pathlib import Path
 
 if __name__ == "__main__":
-    main()
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError:
+        # The above import may fail for some other reason. Ensure that the
+        # issue is really that Django is missing to avoid masking other
+        # exceptions on Python 2.
+        try:
+            import django
+        except ImportError:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            )
+
+        raise
+
+    # This allows easy placement of apps within the interior
+    # family_budget directory.
+    current_path = Path(__file__).parent.resolve()
+    sys.path.append(str(current_path / "familybudget"))
+
+    execute_from_command_line(sys.argv)
